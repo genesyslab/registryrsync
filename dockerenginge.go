1b0653d9b749438477dcdd7c162039c255478e32
Josh Mahowald <joshua.mahowald@gmail.com>
2017-01-09 12:38:36 -0600

    refactoring to fix docker engine

---
+++ dockerenginge.go
@@ -0,0 +1,17 @@
+package main
+
+// "github.com/docker/docker/client"
+
+//
+// type dockerEngine struct {
+// 	cli *client.Client
+// }
+//
+// func NewEngine(eng dockerEngine) {
+// 	cli, err := client.NewEnvClient()
+// 	if err != nil {
+// 		fmt.Errorf("could not create client %v", err)
+// 	}
+// 	eng.cli = cli
+// 	return
+// }
