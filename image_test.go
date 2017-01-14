cd4a6c08063d0d451bab2aee3476bd4f1479851b
Josh Mahowald <joshua.mahowald@gmail.com>
2017-01-14 15:24:59 -0600

    Refactoring to normalize how to express images, interface for docker registry interactions

---
+++ image_test.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetMatchingImages(t *testing.T) {
+	type args struct {
+		reg    Registry
+		filter DockerImageFilter
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    RegistryTargets
+		wantErr bool
+	}{
+		{
+			"simple regex filter",
+			args{
+				mockRegistry{map[string][]string{
+					"alpine":              {"0.1", "stable"},
+					"mynamespace/busybox": {"0.1", "0.1-stable"}}},
+				DockerImageFilter{matchEverything{}, regExFilter(".*stable")}},
+			[]RegistryTarget{RegistryTarget{"alpine", "stable"}, RegistryTarget{"mynamespace/busybox", "0.1-stable"}},
+			false,
+		},
+		{
+			"filter out based off namespaces",
+			args{
+				mockRegistry{map[string][]string{
+					"prod/image1":    {"0.1"},
+					"staging/image1": {"0.1"},
+					"staging/image2": {"0.1"},
+					"dev/image1":     {"0.2"},
+				}},
+				DockerImageFilter{NewNamespaceFilter("staging", "prod"), matchEverything{}}},
+			[]RegistryTarget{RegistryTarget{"prod/image1", "0.1"},
+				RegistryTarget{"staging/image1", "0.1"},
+				RegistryTarget{"staging/image2", "0.1"}},
+			false,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetMatchingImages(tt.args.reg, tt.args.filter)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetMatchingImages() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			sort.Sort(got)
+			sort.Sort(tt.want)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetMatchingImages() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConsolidates(t *testing.T) {
+
+	type args struct {
+		regSource Registry
+		regTarget Registry
+		filter    DockerImageFilter
+		handler   *eventRecorder
+	}
+	tests := []struct {
+		name   string
+		args   args
+		events RegistryEvents
+	}{{"production filters", args{
+		mockRegistry{map[string][]string{
+			"production/tool1": {"0.1", "0.2"},
+			"production/tool2": {"0.1", "latest"},
+		}},
+		mockRegistry{map[string][]string{
+			"production/tool1": {"0.1"},
+		}},
+		DockerImageFilter{NewNamespaceFilter("production"),
+			regExFilter("[\\d\\.]+")},
+		&eventRecorder{},
+	},
+		RegistryEvents{[]RegistryEvent{RegistryEvent{"missing", RegistryTarget{"production/tool1", "0.2"}}, RegistryEvent{"missing", RegistryTarget{"production/tool2", "0.1"}}}},
+	}}
+	for _, tt := range tests {
+		Convey("for consolidation of:"+tt.name, t, func() {
+			Consolidate(tt.args.regSource, tt.args.regTarget, tt.args.filter, tt.args.handler)
+			fmt.Printf("hander %v", tt.args.handler)
+			expected := tt.events.getRegistryTargets()
+			actual := tt.args.handler.events.getRegistryTargets()
+			sort.Sort(expected)
+			sort.Sort(actual)
+			So(actual, ShouldResemble, expected)
+		})
+	}
+}
+
+type eventRecorder struct {
+	events RegistryEvents
+	name   string
+}
+
+func (r *eventRecorder) Handle(evt RegistryEvent) error {
+	r.events.Events = append(r.events.Events, evt)
+	return nil
+}
+
+func regExFilter(pattern string) *RegexTagFilter {
+	f, err := NewRegexTagFilter(pattern)
+	if err != nil {
+		panic("regex:" + pattern + " does not compute")
+	}
+	return f
+}
+
+// Add in Sort interface for registry events so tests have predictable orderings
+func (n RegistryTargets) Len() int {
+	return len(n)
+}
+
+func (n RegistryTargets) Less(i, j int) bool {
+	record1 := n[i]
+	record2 := n[j]
+
+	result := strings.Compare(record1.Repository, record2.Repository)
+	if result == 0 {
+		result = strings.Compare(record1.Tag, record2.Tag)
+	}
+	return result < 0
+
+}
+func (n RegistryTargets) Swap(i, j int) {
+	record1 := n[i]
+	record2 := n[j]
+	n[i] = record2
+	n[j] = record1
+}
---
+++ internal/localregistry.go
@@ -0,0 +1,122 @@
+package internal
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+//StartRegistry - starts a new registry, returning the ip port combination
+//and a closing function that you should use for cleanup.
+//Error of course if there is a problem
+func StartRegistry() (hostIP, port string, deferFn func(), err error) {
+	client, err := docker.NewClientFromEnv()
+	c, err := client.CreateContainer(createOptions())
+	log.Debugf("Container created %+v", c)
+	if err != nil {
+		log.Fatal("Couldn't create even a basic container:", err)
+		return
+	}
+	deferFn = func() {
+		if err = client.RemoveContainer(docker.RemoveContainerOptions{
+			ID:    c.ID,
+			Force: true,
+		}); err != nil {
+			log.Warnf("cannot remove container: %s", err)
+		}
+	}
+	err = client.StartContainer(c.ID, nil)
+	if err != nil {
+		log.Fatalf("Could not start container %v : %s", c, err)
+		deferFn()
+		return
+	}
+	// wait for container to wake up
+	if err = waitStarted(client, c.ID, 5*time.Second); err != nil {
+		deferFn()
+		return
+	}
+	if c, err = client.InspectContainer(c.ID); err != nil {
+		deferFn()
+		return
+	}
+	ports := c.NetworkSettings.Ports
+	//We know that the registry listens on port 5000 generally
+	//but we don't know what it will actually be mapped to when testing
+	p := docker.Port("5000/tcp")
+	if portInfo, ok := ports[p]; ok && len(portInfo) >= 1 {
+		hostIP = portInfo[0].HostIP
+		port = portInfo[0].HostPort
+		//localhost is by default I think marked as
+		//an insecure registry, and since this is just for tests ..
+		if hostIP == "0.0.0.0" {
+			hostIP = "localhost"
+		}
+		if err = waitReachable(hostIP+":"+port, 5*time.Second); err != nil {
+			deferFn()
+			return
+		}
+		return
+	}
+	deferFn()
+	err = fmt.Errorf("Coudln't find port %v in ports %v settings %v", port, ports, c.NetworkSettings)
+	return
+}
+
+// In the case of a single exposed port on a client will
+// return the appropraite connectivity information.  This is
+// needed becasue at least on OSX you CANT use the ips of the containers
+// as of this writing, and docker-machine/boot2docker are no longer
+// the official versions
+
+// waitReachable waits for hostport to became reachable for the maxWait time.
+func waitReachable(hostport string, maxWait time.Duration) error {
+	done := time.Now().Add(maxWait)
+	for time.Now().Before(done) {
+		c, err := net.Dial("tcp", hostport)
+		if err == nil {
+			c.Close()
+			return nil
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	return fmt.Errorf("cannot connect %v for %v", hostport, maxWait)
+}
+
+// waitStarted waits for container to start for the maxWait time.
+func waitStarted(client *docker.Client, id string, maxWait time.Duration) error {
+	done := time.Now().Add(maxWait)
+	for time.Now().Before(done) {
+		c, err := client.InspectContainer(id)
+		if err != nil {
+			//This is to be expected so probably will remove log message later
+			log.Warnf("Container not started %s %s", c, err)
+			break
+		}
+		if c.State.Running {
+			return nil
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	return fmt.Errorf("cannot start container %s for %v", id, maxWait)
+}
+
+func createOptions() docker.CreateContainerOptions {
+	ports := make(map[docker.Port]struct{})
+	hostConfig := &docker.HostConfig{
+		PublishAllPorts: true,
+	}
+	opts := docker.CreateContainerOptions{
+		Config: &docker.Config{
+			Image:        "registry:2.4",
+			PortSpecs:    []string{"5000"},
+			ExposedPorts: ports,
+		},
+		HostConfig: hostConfig,
+	}
+
+	return opts
+}
